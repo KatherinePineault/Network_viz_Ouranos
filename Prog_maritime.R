70b9602e9b0e3521a217f9a62ad2482e2998a72e
KatherinePineault <katherine.pineault@gmail.com>
2017-04-28 14:40:49 -0400

    Codes pour la visualisation des programmes Nordiques, foret, maritime, agriculture, ecobio, tourisme, env bati et santé

--- Prog_maritime.R
+++ Prog_maritime.R
@@ -14,12 +14,13 @@ library("RColorBrewer")
 output_path<- "H:/network_vis/output/"
 data_path<- "H:/network_vis/data/"
 
+
 setwd(data_path)
-nodes<- read.csv("liens_prog_nordique_NODES.csv", sep=";", header=T, as.is=T)
+nodes<- read.csv("liens_prog_maritime_NODES.csv", sep=";", header=T, as.is=T)
 nodes<- nodes[complete.cases(nodes),] ## ajoute parce qu'il y avait des lignes avec des NA a la fin
-links<- read.csv("liens_prog_nordique_EDGES.csv", sep=";", header=T, as.is=T)
+links<- read.csv("liens_prog_maritime_EDGES.csv", sep=";", header=T, as.is=T)
 links<- subset(links, type!="NA") ## ajoute parce qu'il y avait des lignes avec des NA a la fin
-##load("Coordonnees_prog_NRJ.Rda")
+
 
 network<- graph_from_data_frame(d=links, vertices=nodes, directed=F)
 
@@ -40,34 +41,22 @@ V(network)$shape<- ifelse(V(network)$membre==1, "circle", "sphere")
 V(network)$frame.color <- "black"
 
 # Remplacer les noms qui vont sur deux lignes
-   ##V(network_nrj)$label[10]<- "Base de données\ninteractive"
-   ## Voir si c'est possible de ne pas le faire à la main
-V(network)$label[40]<- "Yukon \nCold Climate and \nInnovation center"
-V(network)$label[41]<- "CS Kativik"
-V(network)$label[42]<- "Adm. régionale \nKativik"
-V(network)$label[43]<- "Baffinland \n Iron Mines \nCorp"
-V(network)$label[49]<- "Taquramuit\n Nipingat"
-V(network)$label[56]<-"Lasalle \nConsulting \nGroup"
-V(network)$label[59]<-"Land and sea\nsystem analysis"
-V(network)$label[70]<-"Eeyou\n Marine Region\nWildlife Board"
-V(network)$label[51]<-"Tetra Tek\n EBA"
-V(network)$label[35]<-"Kativik \nTransport"
-V(network)$label[46]<-"Norwegian \nPolar Institute"
-
+##V(network_nrj)$label[10]<- "Base de données\ninteractive"
+## Voir si c'est possible de ne pas le faire à la main
+V(network)$label[45]<-"Regroupement\nPêcheurs professionnels\n sud Gaspésie"
+V(network)$label[40]<-"Atlantic\n  Climate Adaptation\n  Solutions Association"
+V(network)$label[34]<-"Saint-André-de-Kamouraska"
 
 setwd(data_path)
-load("Nordique_coordonnees.Rda")
+load("Maritime_coordonnees.Rda")
 setwd(output_path)
 
-png(file="Visualisation_NOR_27avril_essaicouleurs.png", width=2000, height=2000)
-#pdf("Visualisation_NOR_27avril.pdf")
+#tk_plot<- tkplot(network)
+#l<- tkplot.getcoords(tk_plot)
 
-        #tk_plot<- tkplot(network)
-        #l<- tkplot.getcoords(tk_plot)
+#save(l, file="Maritime_coordonnees.Rda")
 
-#save(l, file="Nordique_coordonnees.Rda")
-
-
+png(file="Visualisation_MARITIME.png", width=2000, height=2000)
 plot(network, 
      vertex.shape=V(network)$shape,
      #vertex.shape="sphere",
@@ -76,13 +65,13 @@ plot(network,
      vertex.label.cex=2.5,
      vertex.label.color="black", 
      layout=l)#, 
-     #main="Programme nordique: Liens et collaborations\n 2014-2016")
+#main="Programme nordique: Liens et collaborations\n 2014-2016")
 
 #legend (x=0, y=-1.1, c("Collaboration", "Contribution en nature ou financière", "Appui", "Suivi"), pch="-",
-       # col=colrs_links[c(1,3,2,4)], ncol=1, pt.cex=1.5, cex=0.7, bty="n")
+# col=colrs_links[c(1,3,2,4)], ncol=1, pt.cex=1.5, cex=0.7, bty="n")
 
 #legend(x=-1.2, y=-1.1, c("Projets Ouranos", "Universités", "Secteur de l'énergie", "Gouvernement", "Organisme subventionnaire", "Consultants", "Autre"), pch=21,
-      # col="black", pt.bg=colrs[c(1,2,6,4,9,7,10)], ncol=1, pt.cex=1, cex=.5,bty="n")
+# col="black", pt.bg=colrs[c(1,2,6,4,9,7,10)], ncol=1, pt.cex=1, cex=.5,bty="n")
 
 #text(x=1.1, y=-.4, "Note 1: Les collaborateurs qui sont\n membres sont représentés par des cercles\n et les non-membres, par des sphères.\n\nNote 2: La taille des projets \nreprésente la taille de leur budget total.", cex=0.5)
 
